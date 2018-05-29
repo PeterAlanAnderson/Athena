67cda9f3c3d22a8b13c80af2b078212710aa5164
hatab002 <hatab002@umn.edu>
2018-05-29 00:02:07 -0500

    Money Model done

---
+++ models/money.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, DataTypes) => {
+    var Money = sequelize.define("Money", {
+        vendorId: {
+            type: Datatypes.INTEGER,
+            primaryKey: true,
+        },
+        balance: {
+            type: Datatypes.INTEGER,
+            allowNull: false, 
+        }
+    });
+
+    Money.associate = (models) => {
+    
+        Money.belongsTo(models.Customer, {
+            foreignKey: 'VendorId'
+        });
+    };
+    return Money;
+}
