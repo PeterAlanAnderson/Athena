6a3468293678712aaee65c05cf4d043cc4605596
Lucas Jensen <lucasjensen56@gmail.com>
2018-05-29 11:21:32 -0500

    added full page carasol section

--- Public/app.js
+++ Public/app.js
@@ -7,6 +7,10 @@
   $(document).ready(function(){
     $('.carousel').carousel();
     $('.modal').modal();
+    $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
 
 
   });
