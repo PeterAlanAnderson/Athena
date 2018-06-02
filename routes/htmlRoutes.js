1e429c478738d7b20ee43468508224be9032a356
Lucas Jensen <lucasjensen56@gmail.com>
2018-06-02 13:01:12 -0500

    fixed htmlRoutes.js file

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -35,9 +35,11 @@ module.exports = function(app) {
   app.get("/allItems", function(req, res) {
     res.render("allItems");
 
+  });
+
   app.get("/createUser", function(req, res) {
     res.render("createUser");
-
   });
 
 };
+
