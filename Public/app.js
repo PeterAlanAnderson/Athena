16dfd4086d94abaa59a76bb3be22fddc7e502e95
Lucas Jensen <lucasjensen56@gmail.com>
2018-06-02 09:57:36 -0500

    final small changes

--- Public/app.js
+++ Public/app.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
   $('.carousel.carousel-slider').carousel({
     fullWidth: true,
     indicators: true,
-    duration: 300
+    duration: 200
   });
    $('.sidenav').sidenav();
    $('.dropdown-trigger').dropdown();
