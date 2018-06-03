3ef72d174eae544af5462c62f8a11e7841a047d7
diorelle <diorelle@screenfeed.com>
2018-06-03 14:11:17 -0500

    pushed data to allItems page

--- Public/allItems.js
+++ Public/allItems.js
@@ -1,25 +1,13 @@
-
-$.get("/api/items", function(data) {
-    console.log("ALL ITEMS");
-    for(var i=0; i < data.length; i++)
-    {
-        console.log(data[i]);  
-    }
-    //$('li').html("")
-})
-
-// $(document).ready(function () {
-//     console.log("here23");
-//     var itemsData = {};
-//     $.ajax("/api/items", {
-//         type: "GET", 
-//         data: ItemsData
+$(function(){
+    $.get("/api/items", function(data) {
+        console.log("ALL ITEMS");
+        for(var i=0; i < data.length; i++)
+        {
+            console.log(data[i]);  
+            $('#cardTitle').html(data[i].name);
+            $('#itemDescription').html(data[i].description);
+            $('#image').attr("src", data[i].image);
+        }
         
-//     }).then(
-//         function() {
-//             console.log("got into the then");
-//             // 
-//             console.log(itemsData);
-//         }
-//     );
-//     });
+    });
+});
