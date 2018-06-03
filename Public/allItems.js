d48c8dcce6c50147bc593d78f6938fea3b796388
diorelle <diorelle@screenfeed.com>
2018-06-03 13:27:08 -0500

    attempting to het handlebars to work

--- Public/allItems.js
+++ Public/allItems.js
@@ -1,3 +1,25 @@
-$(document).ready(function () {
-    console.log("herrre");
-});
+
+$.get("/api/items", function(data) {
+    console.log("ALL ITEMS");
+    for(var i=0; i < data.length; i++)
+    {
+        console.log(data[i]);  
+    }
+    //$('li').html("")
+})
+
+// $(document).ready(function () {
+//     console.log("here23");
+//     var itemsData = {};
+//     $.ajax("/api/items", {
+//         type: "GET", 
+//         data: ItemsData
+        
+//     }).then(
+//         function() {
+//             console.log("got into the then");
+//             // 
+//             console.log(itemsData);
+//         }
+//     );
+//     });
