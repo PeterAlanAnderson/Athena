f8b7250d6da66b24bcbe431f490af58f5d608650
hatab002 <hatab002@umn.edu>
2018-06-04 00:14:59 -0500

    check localStorage for cart

--- Public/cart.js
+++ Public/cart.js
@@ -6,6 +6,12 @@ $(document).ready(function () {
 
 function checkCart () {
     localStorageItems.push(JSON.parse(localStorage.getItem('shoppingCart')));
+    for (j < 0 ; j < localStorageItems.length; j++){
+        $("#cartItemName").val(localStorageItems[i].name);
+        $("#cartItemPrice").val(localStorageItems[i].price)
+        var cartTotal =+ localStorageItems[i].price
+        $("#cartTotal").val(cartTotal)
+    }
 }
 
 $("#addToCart").on("click", function(event) {
@@ -13,19 +19,19 @@ $("#addToCart").on("click", function(event) {
     var id = $(this).data("id");
     console.log(id);
 
-    $.get("/api/items/" + id, function(data) {
-        console.log(data);
-        localStorageItems.push(data);
+    $.get("/api/items/" + id, function(items) {
+        console.log(items);
+        localStorageItems.push(items);
         console.log(localStorageItems);
         var stringifiedArray = [];
         function stringify(){
             for (i = 0; i < localStorageItems.length; i++){
                 stringifiedArray.push(JSON.stringify(localStorageItems[i]));
             }
+            localStorage.setItem('shoppingCart', stringifiedArray);
         };
-        localStorage.setItem('shoppingCart', stringifiedArray);
 
-        res.render("main", data);
+        res.render("main", items);
     })
 });
 })
