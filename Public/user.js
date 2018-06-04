65a1fce641de06371539331e5cac9b2601a6dd8f
unknown <peter.alan.anderson@gmail.com>
2018-06-03 20:20:11 -0500

    items are populating

--- Public/user.js
+++ Public/user.js
@@ -1,3 +1,29 @@
 $(document).ready(function(){
-    
+    const userEmail = sessionStorage.getItem("userEmail")
+    let name = ""
+    let address = "This isn't something we gather at this time"
+    let vendorName = ""
+    let balance = 0
+
+    $.get("/api/customer/"+userEmail, function(data){
+        console.log(data.name)
+        name = data.name;
+        
+        $("#userTitle").text(name)
+        $("#userImage").attr("src","http://cdn.bgr.com/2015/04/strong-bad-email.png")
+        $("#name").text(name)
+        $("#username").text(data.username)
+        $("#address").text(address)
+        $("#email").text(userEmail)
+
+        if (data.vendor){
+            $("#vendorName").text(data.vendorName)
+        } else {
+
+        }
+
+
+    })
+
+
 })
