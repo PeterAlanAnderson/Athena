c81056d75e78b91a93f06b74f80c6c0a093189fe
hatab002 <hatab002@umn.edu>
2018-06-04 21:18:21 -0500

    working on cart

--- Public/allItems.js
+++ Public/allItems.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
             itemCard += data[i].name 
             itemCard +='<i class="material-icons right">more_vert</i></span><button class="btn indigo lighten-2 waves-effect waves-light" id="addToCart" data="'
             itemCard += data[i].id
-            itemCard += '>Add To Cart<i class="material-icons right">add_box</i></button></div>'
+            itemCard += '">Add To Cart<i class="material-icons right">add_box</i></button></div>'
             itemCard += '<div class="card-reveal"><span id="cardTitle" class="card-title grey-text text-darken-4">'    
             itemCard += data[i].name 
             itemCard += '<i class="material-icons right">close</i></span><p id="itemDescription" class="indigo">'
@@ -25,12 +25,55 @@ $(document).ready(function(){
             console.log(itemCard)
 
             $("#itemsContainer").append(itemCard)
-        
+            
             // console.log(data[i]);  
             // $('#cardTitle').html(data[i].name);
             // $('#itemDescription').html(data[i].description);
             // $('#image').attr("src", data[i].image);
         }
         
+    }).then(function(){
+        $("#addToCart").on("click", function (event) {
+            event.preventDefault();
+            var id = $(this).attr("data");
+            console.log(id);
+    
+            $.get("/api/items/" + id, function (items) {
+                console.log(items);
+                localStorageItems.push(items);
+                console.log(localStorageItems);
+                var stringifiedArray = [];
+    
+                function stringify() {
+                    for (let i = 0; i < localStorageItems.length; i++) {
+                        stringifiedArray.push(JSON.stringify(localStorageItems[i]));
+                    }
+                    localStorage.setItem('shoppingCart', stringifiedArray);
+                };
+    
+                // res.render("main", items);
+                checkCart();
+            })
+        });
     });
+    function checkCart() {
+        localStorageItems.push(JSON.parse(localStorage.getItem('shoppingCart')));
+        var cartTotal = 0;
+        if(localStorageItems.length = 0 ){
+            console.log("cart is empty")
+        }else {
+        for (let i = 0; i < localStorageItems.length; i++) {
+            var newCartDiv = $("<div id='listDiv'><img id='imageCart' src=''><ul><li id='cartItemName'><li id='cartItemPrice'>");
+            $('#imageCart').attr("src", localStorageItems[i].image)
+            $("#cartItemName").html(localStorageItems[i].name);
+            $("#cartItemPrice").html(localStorageItems[i].price);
+            cartTotal = +localStorageItems[i].price;
+            $("#cartDiv").append(newCartDiv);
+        }
+        $("#cartTotal").html(cartTotal)
+        console.log(cartTotal)
+    }
+    checkCart();
+    }
+    
 });
--- Public/cart.js
+++ Public/cart.js
@@ -4,40 +4,42 @@ const localStorageItems = [];
 
 $(document).ready(function () {
 
-    function checkCart() {
-        localStorageItems.push(JSON.parse(localStorage.getItem('shoppingCart')));
-        var cartTotal = 0;
-        for (j < 0; j < localStorageItems.length; j++) {
+    // function checkCart() {
+    //     localStorageItems.push(JSON.parse(localStorage.getItem('shoppingCart')));
+    //     var cartTotal = 0;
+    //     for (j < 0; j < localStorageItems.length; j++) {
+    //         var newCartDiv = $("<div id='listDiv'><img id='imageCart' src=''><ul><li id='cartItemName'><li id='cartItemPrice'>");
+    //         $('#imageCart').attr("src", localStorageItems[i].image)
+    //         $("#cartItemName").html(localStorageItems[i].name);
+    //         $("#cartItemPrice").html(localStorageItems[i].price);
+    //         cartTotal = +localStorageItems[i].price;
+    //         $("#cartDiv").append(newCartDiv);
+    //     }
+    //     $("#cartTotal").val(cartTotal)
+    // }
 
-            $("#cartItemName").val(localStorageItems[i].name);
-            $("#cartItemPrice").val(localStorageItems[i].price);
-            cartTotal = +localStorageItems[i].price;
-        }
-        $("#cartTotal").val(cartTotal)
-    }
+    // $("#addToCart").on("click", function (event) {
+    //     event.preventDefault();
+    //     var id = $(this).attr("data");
+    //     console.log(id);
 
-    $("#addToCart").on("click", function (event) {
-        event.preventDefault();
-        var id = $(this).attr("data");
-        console.log(id);
+    //     $.get("/api/items/" + id, function (items) {
+    //         console.log(items);
+    //         localStorageItems.push(items);
+    //         console.log(localStorageItems);
+    //         var stringifiedArray = [];
 
-        $.get("/api/items/" + id, function (items) {
-            console.log(items);
-            localStorageItems.push(items);
-            console.log(localStorageItems);
-            var stringifiedArray = [];
+    //         function stringify() {
+    //             for (i = 0; i < localStorageItems.length; i++) {
+    //                 stringifiedArray.push(JSON.stringify(localStorageItems[i]));
+    //             }
+    //             localStorage.setItem('shoppingCart', stringifiedArray);
+    //         };
 
-            function stringify() {
-                for (i = 0; i < localStorageItems.length; i++) {
-                    stringifiedArray.push(JSON.stringify(localStorageItems[i]));
-                }
-                localStorage.setItem('shoppingCart', stringifiedArray);
-            };
-
-            res.render("main", items);
-            // checkCart();
-        })
-    });
+    //         // res.render("main", items);
+    //         // checkCart();
+    //     })
+    // });
 
     //************* post purchase log on checkout
 
