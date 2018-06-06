b675ff32dc115b44449e3fe1a1cac03824a41a3d
hatab002 <hatab002@umn.edu>
2018-06-05 21:38:36 -0500

    added "add to cart" functionality to the searchItems.js

--- Public/searchItems.js
+++ Public/searchItems.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
             itemCard += '"></a></div>'
             itemCard += '<div class="card-content"><span class="card-title activator grey-text text-darken-4">'
             itemCard += data[i].name 
-            itemCard +='<i class="material-icons right">more_vert</i></span><button class="btn indigo lighten-2 waves-effect waves-light" id="addToCart" data="'
+            itemCard +='<i class="material-icons right">more_vert</i></span><button class="btn indigo lighten-2 waves-effect waves-light addToCart" data="'
             itemCard += data[i].id
             itemCard += '>Add To Cart<i class="material-icons right">add_box</i></button></div>'
             itemCard += '<div class="card-reveal"><span id="cardTitle" class="card-title grey-text text-darken-4">'    
@@ -34,5 +34,38 @@ $(document).ready(function(){
             // $('#image').attr("src", data[i].image);
         }
         
+    }).then(function(){
+        $(".addToCart").on("click", function (event) {
+            event.preventDefault();
+            var id = $(this).attr("data");
+            console.log(id);
+    
+            $.get("/api/items/" + id, function (data) {
+                console.log(data);
+                var cartItem = {
+                    id: data.id,
+                    name: data.name,
+                    price: data.price,
+                    imageUrl: data.image,
+                    owner: data.owner
+                }
+                localStorageItems.push(cartItem);
+                console.log(`cart item: ${cartItem.id}, ${cartItem.name}, ${cartItem.price}, ${cartItem.imageUrl}`)
+                console.log(`local storage array: ${localStorageItems}`);
+                var stringifiedArray = [];
+    
+                function stringify() {
+                    for (let i = 0; i < localStorageItems.length; i++) {
+                        stringifiedArray.push(JSON.stringify(localStorageItems[i]));
+                        console.log(`stringified array : ${stringifiedArray}`)
+                        localStorage.setItem('shoppingCart', stringifiedArray);
+                        console.log("local storage: " + localStorage.shoppingCart)
+                    }
+                    
+                };
+                stringify();
+    
+            })
+        });
     });
 });
